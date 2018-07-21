6ba70a7c41e740d4491108440e0f25cb204380f2
championcp <championcp@hotmail.com>
2018-07-21 03:05:48 +0000

    commit c++ file

---
+++ c++/HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, i;
+    cin >> n;
+    for (i = 0; i < n; i++) {
+        cout << setw(13 * i) << setiosflags(ios::left) << " " << "hello world!" << endl;
+    }
+    return 0;
+}
